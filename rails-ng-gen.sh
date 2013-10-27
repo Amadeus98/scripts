0ac5ad3a936a71b979824bfe06c6631f0b45dc1e
Emmanuel Marcha <emarcha@hawk.iit.edu>
2013-10-27 10:50:52 -0500

    Update variable assignment and move sudo usage

--- rails-ng-gen.sh
+++ rails-ng-gen.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # Basic Rails + Angular scaffolding generator 
 # Assuming Rails, Node, Compass, Yeoman, Angular Generator, 
-# and Grunt CLI are already installed 
 
-$APP_NAME = $1
-$PATH     = $2
+APP_NAME=$1
+APP_PATH=$2
 
 # Generate Rails API and copy over files 
-rails-api new $PATH/$APP_NAME --skip-test-unit
-cp default-gemfile $PATH/$APP_NAME/Gemfile
+rails-api new $APP_PATH/$APP_NAME --skip-test-unit
+cp default-gemfile $APP_PATH/$APP_NAME/Gemfile
 
 # cd into directory and begin Rails-related installs
-cd $PATH/$APP_NAME
-bundle install 
+cd $APP_PATH/$APP_NAME
+bundle install --without production 
 rails g rspec:install 
 rails g figaro:install 
 rails g devise:install 
@@ -20,5 +19,5 @@ rails g devise:install
 # Create client directory and begin Angular-related intalls 
 mkdir client/
 cd client/
-sudo yo angular $APP_NAME
-npm install --save-dev grunt-connect-proxy
+yo angular $APP_NAME
+sudo npm install --save-dev grunt-connect-proxy
