79cba8efd706d7ed90afe9d66fdab1ededd35363
Emmanuel Marcha <emarcha@hawk.iit.edu>
2013-10-27 08:54:01 -0500

    Remove model generation

--- rails-ng-gen.sh
+++ rails-ng-gen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Basic Rails + Angular scaffolding generator 
-# Assuming Rails, Node, Compass and other things are already installed 
+# Assuming Rails, Node, Compass, Yeoman, Angular Generator, 
+# and Grunt CLI are already installed 
 
 $APP_NAME = $1
 $PATH     = $2
@@ -9,23 +10,10 @@ $PATH     = $2
 rails-api new $PATH/$APP_NAME --skip-test-unit
 cp default-gemfile $PATH/$APP_NAME/Gemfile
 
-# cd into directory and begin installs
+# cd into directory and begin Rails-related installs
 cd $PATH/$APP_NAME
 bundle install 
 rails g rspec:install 
 rails g figaro:install 
 rails g devise:install 
 
-# Begin model generation 
-rails g devise user 
-rails g model authorization \ 
-              provider:string \ 
-              uid:string \
-              user_id:integer \ 
-              token:string \ 
-              secret:string \ 
-              name:string \ 
-              url:string
-# Copy user file 
-# Add relationships 
-rake db:migrate 
